c5a09548faf7e6077be39a2d72527d4011c17fe2
Rohan Sharma <68840167+Sustainability4@users.noreply.github.com>
2022-03-04 16:54:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,20 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# We need to mention the project name, just make sure the project name is unique. 
+# user name here has been added as the username for github repository. We decided with this username as we need to access the same in our URLs 
+# 
+    
 PKG_NAME = "oneNeuron_pkg"
-USER_NAME = "c17hawke"
+USER_NAME = "sustainability4"
 PROJECT_NAME = "oneNeuron-pkg"
 
 setuptools.setup(
     name=f"{PKG_NAME}-{USER_NAME}",
     version="0.0.3",
     author=USER_NAME,
-    author_email="sunny.c17hawke@gmail.com",
+    # provide the name of the author 
+    author_email="rohan.prospects@gmail.com",
     description="A small package for perceptron",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -26,7 +31,9 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
+    # Please make the python requirements liberal in a sense that it does not throw unnecessary errors
     python_requires=">=3.6",
+    # install requirements are just a copy paste job from your requirements.txt file
     install_requires=[
         "numpy==1.21.4",
         "pandas==1.3.4",
